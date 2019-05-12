5f02309a1eb504ac235d65e97e0c914c734e06c7
Martin Lim <martin.lim@duke.edu>
2019-05-11 23:19:33 -0700

    rawImg process

--- script.py
+++ script.py
@@ -1,11 +1,63 @@
 from PIL import Image
-import cv2, numpy
+import cv2, random, os
+import numpy as np
 
-img = cv2.imread("C:/Users/Martin/git/AVRS/data/test.jpg")
+img = Image.open("C:/Users/Martin/git/AVRS/data/test.jpg")
+bg = Image.open("C:/Users/Martin/Pictures/backgrounds/darkgreen.jpg")
 
-h, w, l = numpy.array(img).shape
+im = bg.copy()
+im.paste(img, (0, 0))
 
-vid = cv2.VideoWriter("demo.avi", cv2.VideoWriter_fourcc(*'DIVX'), 10, (w, h))
+im.show()
 
-vid.write(img)
+h, w, l = np.array(img).shape
 
+
+vid = cv2.VideoWriter("demo.avi", cv2.VideoWriter_fourcc(*'DIVX'), 10, (1280, 720))
+
+vid.release()
+
+
+
+def imgProcess(rawImages):
+	#load in backgrounds
+	backs = []
+	path = "C:/Users/Martin/Pictures/backgrounds"
+	for filename in os.listdir(path):
+		back_path = os.path.join(path, filename)
+		back = Image.open(back_path)
+		backs.append(back)
+	
+	#process pics and add background
+	ret = []
+	for image in rawImages:
+		h, w, l = np.array(image).shape
+
+		if w >= 1100:
+			ratio = h/w 
+			w = 1000
+			h = int(1000 * ratio)
+			image.resize((1000, h))
+
+		xcord = (1280 - w) // 2
+		ycord = (720 - h) // 2
+
+		rando = random.randint(0, len(backs))
+		bcopy = backs[rando].copy()
+		bcopy.paste(image, (xcord, ycord))
+		ret.append(bcopy)
+	#return array of final images on random backgrounds
+	return ret
+
+def imgFromFile(path):
+	ret = []
+	for filename in os.listdir(path):
+		if filename[-4:] == ".jpg":
+			img_path = os.path.join(path, filename)
+			img = Image.open(img_path)
+			ret.append(img)
+	return ret
+		
+		
+
+		
